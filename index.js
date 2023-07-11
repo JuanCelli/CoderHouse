122f5731aaf77cd1c06b0657f965af348778beab
JuanCelli <juanicelli@gmail.com>
2023-07-10 23:09:02 -0300

    Agrego filtro categorias

--- index.js
+++ index.js
@@ -1,54 +1,51 @@
 let productsCart = []
 
-function productSwitch(id){
-    let productPrice
-    let productName
-    let dataProduct
-    switch (id) {
-        case 1:
-            productName = "Jean Negro"
-            productPrice = 6500
-            break;
-        case 2:
-            productName = "Campera Parka"
-            productPrice = 18000            
-            break;
-        case 3:
-            productName = "Chomba"
-            productPrice = 3500                        
-            break;
-        case 4:
-            productName = "Zapatillas"
-            productPrice = 37999                        
-            break;
-        case 5:
-            productName = "Camiseta de Fútbol"
-            productPrice = 17599                        
-            break;
-        case 6:
-            productName = "Gorro de Lana"
-            productPrice = 4299                        
-            break;
-            
-        default:
-            dataProduct = false
-            break;
+class product {
+    constructor(name,id,price,type,img){
+        this.name = name
+        this.id = id
+        this.price = price
+        this.type = type
+        this.img = img
     }
-        
-    if(dataProduct!==false){
-        dataProduct = [productName,productPrice]
-    }
-    return dataProduct
 }
+const products =[
+    new product("Jean Negro",1,6500,"Pantalones","https://elasdeljean.com/wp-content/uploads/2020/05/15883370602464ce466dd5f3dfc9b448444cc10ef3.png"),
+    new product("Campera Parka",2,18000,"Camperas","https://http2.mlstatic.com/D_NQ_NP_837023-MLA69580204294_052023-O.webp"),
+    new product("Chomba",3,3500,"Remeras","https://biffas.com.ar/wp-content/uploads/2021/09/chomba-lima-marino.jpg"),
+    new product("Zapatillas",4,37999,"Calzado","https://www.grupooxicas.com/242-thickbox_default/zapatilla-calzado-de-seguridad-funcional-mujer-bali-numeros-del-34-al-42.jpg   "),
+    new product("Camiseta de Fútbol",5,17599,"Remeras","https://www.thefutbolstore.com.ar/uploads/v2/product/hi/202006_700_A.jpg"),
+    new product("Gorro de Lana",6,4299,"Accesorios","https://celadasa.vtexassets.com/arquivos/ids/216499-800-auto?v=637993807990500000&width=800&height=auto&aspect=true")
+]
 
+function productSwitch(id){
+    const dataProduct={
+        status: false,
+        name: undefined,
+        price: undefined
+    }
+
+
+    products.forEach(product => {
+        if(product.id===id){
+            dataProduct.status=true
+            dataProduct.name = product.name
+            dataProduct.price = product.price
+            return dataProduct
+        }
+    })
+    
+    return dataProduct;
+}
 
 function addProduct(){
     let productId = parseInt(prompt("Ingrese el ID del producto que desea agregar."))
-    let dataProduct = productSwitch(productId)
+    let productSelected = productSwitch(productId)
+    console.log(productSelected)
     
-    if(dataProduct!==false){
-        alert(`Has seleccionado ${dataProduct[0]} que tiene valor de $${dataProduct[1]}`)
-        loadInCart(dataProduct)
+    if(productSelected.status!==false){
+        alert(`Has seleccionado ${productSelected.name} que tiene valor de $${productSelected.price}`)
+        loadInCart(productSelected)
     }
     else{
         alert(`No has ingresado un ID valido, vuelve a intentarlo.`)
@@ -68,7 +65,7 @@ function removeOfCart(){
     let productRemoved = productsCart.pop()
 
     if(productRemoved!==undefined){
-        alert(`Has quitado ${productRemoved[0]} del carrito correctamente.`)
+        alert(`Has quitado ${productRemoved.name} del carrito correctamente.`)
     }
     else{
         alert(`El carrito está vacio.`)
@@ -85,11 +82,61 @@ function calTotal(){
     }
     else{
         for(let i=0; i < productsCart.length; i++){
-            console.log(`Producto: ${productsCart[i][0]} // Valor: $${productsCart[i][1]}`)
-            cartPriceTotal+=productsCart[i][1]
+            console.log(`Producto: ${productsCart[i].name} // Valor: $${productsCart[i].price}`)
+            cartPriceTotal+=productsCart[i].price
         }
         console.log(`Valor Total: $${cartPriceTotal} // Cantidad de productos: ${productsCart.length}`)
         alert(`Valor Total: $${cartPriceTotal} // Cantidad de productos: ${productsCart.length}`)
     }
 }
+
+function componentProduct(container,nameProduct,idProduct,priceProduct,img){
+    const containerProducts= document.querySelector("."+container) 
+    containerProducts.innerHTML =   containerProducts.innerHTML+
+                                    `<div class="product-card">
+                                        <img class="product-img" src=${img} alt=${nameProduct}>
+                                        <div class="product-info-container">
+                                            <h2 class="product-name">${nameProduct}</h2>
+                                            <p class="product-id product-info-item">ID: ${idProduct}</p>
+                                            <p class="product-price product-info-item">Valor: $${priceProduct}</p>
+                                        </div>
+                                    </div>`
+}
+
+function renderProducts(filter){
+    if(filter!==undefined){
+        filter = filter.toString()
+    }
+    unrenderProducts()
+    const container = "main-products-container"
+    products.forEach(product => {
+        if(product.type===filter||filter===undefined){
+            componentProduct(container,product.name,product.id,product.price,product.img)
+        }
+    });
+}
+
+function renderFilterTypeSelector(){
+    const container = document.querySelector(".main-category-container")
+    const typeAlreadyRender = []
+
+    container.innerHTML += `<li onclick="renderProducts()" class="category-item">Todo</li>`
+
+    products.forEach(product=>{
+        if(!typeAlreadyRender.includes(product.type)){
+            container.innerHTML += `<li onclick="renderProducts('${product.type}')" class="category-item">${product.type}</li>`
+            typeAlreadyRender.push(product.type)
+        }
+    })
+}
+
+function unrenderProducts(){
+    const container = document.querySelector(".main-products-container")
+    while (container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+}
+
+renderFilterTypeSelector()
+renderProducts()
 
